21d3627cc2475b1041b95b75216911dff7034dc2
chewxinning16 <chewxinning16@gmail.com>
2025-05-28 13:28:48 +0800

    update styling for mobile view menu

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ function toggleMenu() {
     // get the menu and icon elements
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
-    
-    // toggle the 'open' class on both elements
-    menu.classList.toggle('open');
-    icon.classList.toggle('open');
+
+    // toggle or untoggle the 'open' class on both elements
+    const isOpen = menu.classList.contains('open');
+    if (isOpen) {
+        menu.classList.remove('open');
+        icon.classList.remove('open');
+    } else {
+        menu.classList.add('open');
+        icon.classList.add('open');
+    }
 }
