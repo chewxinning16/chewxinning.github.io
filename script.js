ea02f4d148fa285ccb3e6091923d5713c6c4c5f9
chewxinning16 <chewxinning16@gmail.com>
2025-05-29 12:31:16 +0800

    final edit on overall content and styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+/* to handle the hamburger menu toggle functionality */
 function toggleMenu() {
-    // a function to toggle or untoggle the menu and icon state
 
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
@@ -14,36 +14,10 @@ function toggleMenu() {
     }
 }
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("slides");
-//     let dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) { slideIndex = 1 }
-//     if (n < 1) { slideIndex = slides.length }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";  
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
-// }
-
+/* slideshow functionality for two project sections */
 let slideIndex = [1, 1];
-let slideId = ["slide-1", "slide-2"];
-let dotId = ["dot-1", "dot-2"];
+const slideId = ["slide-1", "slide-2"];
+const dotId = ["dot-1", "dot-2"];
 showSlides(1, 0);
 showSlides(1, 1);
 
